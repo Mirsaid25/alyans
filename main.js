5d568fc103def4bc23563d04fe7d8f6f500cca20
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-11-18 12:45:03 +0500

    add anythink

--- main.js
+++ main.js
@@ -1,93 +1,96 @@
-let accordion_child = document.querySelectorAll(".accordion_child");
+let accordion = document.querySelectorAll(".accordion");
 
-for (let item of accordion_child) {
-    item.addEventListener("click", function () {
-        // let icon =
-        if (item.classList.contains("accordion_style")) {
-            item.querySelector("#accordion_icon").style.transform =
-                "rotate(180deg)";
-            item.querySelector(".title").style.color = "#8859CF";
-            this.classList.replace("accordion_style", "accordion_active");
-        } else {
-            item.querySelector("#accordion_icon").style.transform =
-                "rotate(0deg)";
-            item.querySelector(".title").style.color = "#333333";
-            this.classList.replace("accordion_active", "accordion_style");
-        }
-    });
-}
+accordion.forEach((el) => {
+    el.addEventListener("click", () => {
+		let content = el.nextElementSibling;
+		let icon = el.querySelector(".accordion_icon")
+		let text = el.querySelector(".accordion_text")
+		
+		if(content.style.maxHeight){
+			document.querySelectorAll(".content").forEach((el)=> el.style.maxHeight = null)
+			icon.style.transform = "rotate(0deg)"
+			text.style.color = "#333333"
+		}else{
+			document.querySelectorAll(".content").forEach((el)=> el.style.maxHeight = null)
+			document.querySelectorAll(".accordion_icon").forEach((el)=> el.style.transform = "rotate(0deg)")
+			document.querySelectorAll(".accordion_text").forEach((el)=> el.style.color = "#333333")
+			text.style.color = "#8859CF"
+			content.style.maxHeight = content.scrollHeight + "px"
+			icon.style.transform = "rotate(180deg)"
+		}
+	});
+});
 
 // ***************** swiper *****************************
 
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
     speed: 400,
     breakpoints: {
         // when window width is >= 320px
         320: {
-          slidesPerView: 2,
-          spaceBetween: 20
+            slidesPerView: 2,
+            spaceBetween: 20,
         },
         // when window width is >= 480px
         480: {
-          slidesPerView: 3,
-          spaceBetween: 30
+            slidesPerView: 3,
+            spaceBetween: 30,
         },
         // when window width is >= 640px
         640: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        }
-      }
-  });
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
 
-  const swiper_block = document.querySelector('.swiper').swiper;
-  let prevButton = document.querySelector(".button-prev")
-  let nextButton = document.querySelector(".button-next")
+const swiper_block = document.querySelector(".swiper").swiper;
+let prevButton = document.querySelector(".button-prev");
+let nextButton = document.querySelector(".button-next");
 
 //   prevButton.onclick(swiper_clock.slidePrev())
-    prevButton.addEventListener("click", function(e){
-        swiper_block.slidePrev()
-    })
+prevButton.addEventListener("click", function (e) {
+    swiper_block.slidePrev();
+});
 
-    nextButton.addEventListener("click", function(e){
-        swiper_block.slideNext()
-    })
+nextButton.addEventListener("click", function (e) {
+    swiper_block.slideNext();
+});
 
 // ********************** swiper2**********************************
 
-
-const swiper_2 = new Swiper('.swiper_2', {
+const swiper_2 = new Swiper(".swiper_2", {
     speed: 400,
     breakpoints: {
         // when window width is >= 320px
         320: {
-          slidesPerView: 2,
-          spaceBetween: 20
+            slidesPerView: 2,
+            spaceBetween: 20,
         },
         // when window width is >= 480px
         480: {
-          slidesPerView: 3,
-          spaceBetween: 30
+            slidesPerView: 3,
+            spaceBetween: 30,
         },
         // when window width is >= 640px
         640: {
-          slidesPerView: 4,
-          spaceBetween: 30
-        }
-      }
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
 });
 
-  const swiper_block2 = document.querySelector('.swiper_2').swiper;
-  let prevButton_2 = document.querySelector(".button-prev_2")
-  let nextButton_2 = document.querySelector(".button-next_2")
+const swiper_block2 = document.querySelector(".swiper_2").swiper;
+let prevButton_2 = document.querySelector(".button-prev_2");
+let nextButton_2 = document.querySelector(".button-next_2");
 
 //   prevButton.onclick(swiper_clock.slidePrev())
-    prevButton_2.addEventListener("click", function(e){
-        swiper_block2.slidePrev()
-    })
+prevButton_2.addEventListener("click", function (e) {
+    swiper_block2.slidePrev();
+});
 
-    nextButton_2.addEventListener("click", function(e){
-        swiper_block2.slideNext()
-    })
+nextButton_2.addEventListener("click", function (e) {
+    swiper_block2.slideNext();
+});
