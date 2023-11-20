d0e9e776a9d20cdb5834541c64e45042bc4df97b
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-11-20 14:14:41 +0500

    add: modal

--- main.js
+++ main.js
@@ -108,3 +108,38 @@ prevButton_2.addEventListener("click", function (e) {
 nextButton_2.addEventListener("click", function (e) {
     swiper_block2.slideNext();
 });
+
+// ********************* burger_menu ****************************
+
+let burger_menu = document.querySelector(".burger_menu")
+let menu_close = document.querySelector("#menu_close")
+let menu_open = document.querySelector("#menu_open")
+
+menu_open.addEventListener("click", ()=>{
+    burger_menu.classList.replace("hidden", "block")
+    document.body.style.overflowY = "hidden";
+})
+
+menu_close.addEventListener("click", ()=>{
+    burger_menu.classList.replace("block", "hidden")
+    document.body.style.overflowY = "scroll";
+})
+
+// ********************* modal **********************************
+
+let modal = document.querySelector(".modal")
+let modal_close = modal.querySelector("#modal_close")
+let modal_open = document.querySelectorAll(".modal_open")
+
+modal_open.forEach(item =>{
+    item.addEventListener("click", ()=>{
+        modal.classList.replace("hidden", "block")
+        burger_menu.classList.replace("block", "hidden")
+        document.body.style.overflowY = "hidden";
+    })
+})
+
+modal_close.addEventListener("click", ()=>{
+    modal.classList.replace("block", "hidden")
+    document.body.style.overflowY = "scroll";
+})
