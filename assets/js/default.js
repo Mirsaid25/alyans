38c233cfcc5b74a20d008cb8d2ed056ddc5bc653
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-12-05 14:27:19 +0500

    all ready

---
+++ assets/js/default.js
@@ -0,0 +1,95 @@
+// ********************* burger_menu ****************************
+
+let burger_menu = document.querySelector(".burger_menu")
+let menu_close = document.querySelector("#menu_close")
+let menu_open = document.querySelector("#menu_open")
+
+menu_open.addEventListener("click", ()=>{
+    burger_menu.classList.replace("hidden", "block")
+    document.body.style.overflowY = "hidden";
+})
+
+menu_close.addEventListener("click", ()=>{
+    burger_menu.classList.replace("block", "hidden")
+    document.body.style.overflowY = "scroll";
+})
+
+// ********************* modal **********************************
+
+let modal = document.querySelector(".modal")
+let modal_close = modal.querySelector("#modal_close")
+let modal_open = document.querySelectorAll(".modal_open")
+
+modal_open.forEach(item =>{
+    item.addEventListener("click", ()=>{
+        modal.classList.replace("hidden", "block")
+        burger_menu.classList.replace("block", "hidden")
+        document.body.style.overflowY = "hidden";
+    })
+})
+
+modal_close.addEventListener("click", ()=>{
+    modal.classList.replace("block", "hidden")
+    document.body.style.overflowY = "scroll";
+})
+
+// *********************** Anchor links **********************************
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substring(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+// ************************* header:hover **************************************
+
+const submenu = document.querySelectorAll(".submenu")
+const submenu_icon = document.querySelectorAll(".submenu_icon")
+const submenu_block = document.querySelectorAll(".submenu_block")
+
+submenu.forEach((item, index )=> {
+  item.onmouseenter = () => {
+    submenu_icon[index].classList.replace("rotate-180", "rotate-0")
+    submenu_block[index].classList.replace("hidden", "block")
+  }
+
+  item.onmouseleave = () => {
+    submenu_icon[index].classList.replace("rotate-0","rotate-180")
+    submenu_block[index].classList.replace("block", "hidden")
+  }
+})
+
+// submenu.onmouseenter = () => {
+//   submenu_icon.classList.replace("rotate-180", "rotate-0")
+//   submenu_block.classList.replace("hidden", "block")
+// }
+
+submenu.onmouseleave = () => {
+  submenu_icon.classList.replace("rotate-0","rotate-180")
+  submenu_block.classList.replace("block", "hidden")
+}
+
+// ************************* header:hover **************************************
+
+const submenu_mobile = document.querySelectorll(".submenu_mobile")
+const submenu_icon_mobile = document.querySelector(".submenu_icon_mobile")
+const submenu_block_mobile = document.querySelector(".submenu_block_mobile")
+
+submenu_mobile.onfocus = () => {
+  submenu_icon_mobile.classList.replace("rotate-180", "rotate-0")
+  submenu_block_mobile.classList.replace("hidden", "block")
+}
+
+submenu_mobile.onblur = () => {
+  submenu_icon_mobile.classList.replace("rotate-0","rotate-180")
+  submenu_block_mobile.classList.replace("block", "hidden")
+}
---
+++ assets/js/services.js
@@ -0,0 +1,57 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
+const swiper = new Swiper(".swiper_4", {
+    speed: 400,
+    loop:true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        624: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
+
+const swiper_block = document.querySelector(".swiper_4").swiper;
+let prevButton = document.querySelector(".button-prev");
+let nextButton = document.querySelector(".button-next");
+
+prevButton.addEventListener("click", function (e) {
+    swiper_block.slidePrev();
+});
+
+nextButton.addEventListener("click", function (e) {
+    swiper_block.slideNext();
+});
+
+// *********************************************************
+
+const swiperChild = document.querySelectorAll(".revInnerSlider")
+
+for(let i = 1; i <= swiperChild.length; i++){
+    const swiper2 = new Swiper(`.swiperChild_${i}`, {
+        speed: 400,
+        loop:true,
+        navigation: {
+            nextEl: `.button-next_child_${i}`,
+            prevEl: `.button-prev_child_${i}`,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+            },
+        },
+    });
+}
